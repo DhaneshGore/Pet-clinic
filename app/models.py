20267fda314817432179b4a7a1c1516c90ddb5b0
DhaneshGore <dhanesh9765@gmail.com>
2024-11-08 23:49:00 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -3,12 +3,13 @@ from app import db
 class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    contact = db.Column(db.String(20))
     pets = db.relationship('Pet', backref='owner', lazy=True)
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    type = db.Column(db.String(80))  # This should be here
+    type = db.Column(db.String(80), nullable=False)  
     owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
 
 
